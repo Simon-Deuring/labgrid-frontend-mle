f2fadacd077d5987da33503758b511ea44305116
Simon Deuring <simon.deuring@uni-ulm.de>
2021-10-28 20:30:41 +0200

    Install Autobahn to connect to the coordinator

--- labgrid-web-server/server.js
+++ labgrid-web-server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var autobahn = require('autobahn');
+
+// REST methods
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
