092df9144c3e18a4c9753d014a69c0beddc91544
Simon Deuring <simon.deuring@uni-ulm.de>
2021-11-01 11:31:33 +0100

    Add code for test connection

--- labgrid-web-server/server.js
+++ labgrid-web-server/server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 
 var autobahn = require('autobahn');
+var connection = new autobahn.Connection({
+   url: "ws://127.0.0.1:8080/ws",
+   realm: "realm1"
+});
+connection.onopen = function (session, details) {
+   console.log('Connection successfully accomplished')
+};
+connection.open();
 
 // REST methods
 app.get('/', function (req, res) {
