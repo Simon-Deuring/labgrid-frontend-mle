1eec23cc65653e34a3bd0140ba4022bb176a5d42
Simon Deuring <simon.deuring@uni-ulm.de>
2021-11-02 11:26:38 +0100

    Add code for remote procedure calls

--- labgrid-web-server/server.js
+++ labgrid-web-server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var places;
+var resources;
+
 var autobahn = require('autobahn');
 var connection = new autobahn.Connection({
    url: "ws://127.0.0.1:8080/ws",
@@ -9,6 +12,18 @@ var connection = new autobahn.Connection({
 connection.onopen = function (session, details) {
    console.log('Connection successfully accomplished')
 };
+connection.onopen = function (session, details) {
+   // Request places from coordinator
+   session.call('org.labgrid.coordinator.get_places').then(res => places = res);
+   //places = await session.call('org.labgrid.coordinator.get_places');
+
+   // Request hardware resources from coordinator
+   session.call('org.labgrid.coordinator.get_resources').then(res => resources = res);
+   //resources = await session.call('org.labgrid.coordinator.get_places');
+};
+connection.onclose = function (reason, details) {
+   // handle connection lost
+}
 connection.open();
 
 // REST methods
