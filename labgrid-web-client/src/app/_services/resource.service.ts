bd19a86eb5995fe8cf7b723b5a258badb99a867b
Simon-Deuring <simon@deuring.info>
2021-12-12 15:39:34 +0100

    Import autobahn and connect to router

--- labgrid-web-client/src/app/_services/resource.service.ts
+++ labgrid-web-client/src/app/_services/resource.service.ts
@@ -2,12 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Resource } from '../../models/resource';
 
+import * as autobahn from 'autobahn-browser';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ResourceService {
 
-  constructor(private _http: HttpClient) { }
+  private session: any;
+
+  constructor(private _http: HttpClient) {
+    const connection = new autobahn.Connection({
+      url: 'ws://localhost:8083/ws',
+      realm: 'frontend'
+    });
+
+    let service = this;
+    connection.onopen = async function (session: any, details: any) {
+      service.session = session;
+    }
+
+    connection.open();
+  }
 
   public async getResources(): Promise<Resource[]> {
     const resources = await this._http.get('../assets/resources.json').toPromise() as Resource[];
