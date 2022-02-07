1bdfec126d0717803c2dc6b9c19351eed889c519
Simon-Deuring <simon@deuring.info>
2022-02-07 14:38:47 +0100

    Add login route

--- labgrid-web-client/src/app/app-routing.module.ts
+++ labgrid-web-client/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 import { PlaceComponent } from './place/place.component';
+import { PlaceOverviewComponent } from './place-overview/place-overview.component';
 import { ResourceComponent } from './resource/resource.component';
 import { ResourceOverviewComponent } from './resource-overview/resource-overview.component';
-import { PlaceOverviewComponent } from './place-overview/place-overview.component';
 
 const routes: Routes = [
     { path: '', component: PlaceOverviewComponent },
     { path: 'place/:placename', component: PlaceComponent },
     { path: 'resourceOverview', component: ResourceOverviewComponent },
     { path: 'resource/:resourceName', component: ResourceComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', component: ErrorComponent },
 ];
 
