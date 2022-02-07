af74f2bdd23c96d40c95c106ceaf0a16894d84e5
Simon-Deuring <simon@deuring.info>
2022-02-07 15:47:17 +0100

    Implement minimal login form

--- labgrid-web-client/src/app/login/login.component.ts
+++ labgrid-web-client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../auth/login.service';
 
 @Component({
     selector: 'app-login',
@@ -6,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
-
     ngOnInit(): void {}
+
+    message: string;
+
+    constructor(public ls: LoginService, private router: Router) {
+        this.message = this.getMessage();
+    }
+
+    getMessage() {
+        return 'Logged ' + (this.ls.isLoggedIn ? 'in' : 'out');
+    }
+
+    login() {
+        this.message = 'Trying to log in ...';
+
+        this.ls.login();
+        this.message = this.getMessage();
+    }
+
+    logout() {
+        this.ls.logout();
+        this.message = this.getMessage();
+    }
 }
