b2d59be39cb3481b5aae6d853fb8aedb14541423
Kevin Grüninger <kevin.grueninger>
2022-03-08 14:19:51 +0100

    integrated prompty

--- python-wamp-client/labby/__main__.py
+++ python-wamp-client/labby/__main__.py
@@ -6,22 +6,22 @@ Easy labby launch script
 import argparse
 import json
 from os import getenv, path
-
 from labby import run_router
+import labby.prompty as prompty
 
 if __name__ == '__main__':
 
     CONFIG_PATH = "./.labby_config.json"
     config = {
-        "backend_url": u"ws://localhost:20408/ws",
+        "backend_url": "ws://localhost:20408/ws",
         "backend_realm": "realm1",
-        "frontend_url": u"ws://localhost:8083/ws",
+        "frontend_url": "ws://localhost:8083/ws",
         "frontend_realm": "frontend",
         "exporter" : None
     }
 
     if path.exists(CONFIG_PATH):
-        with open(CONFIG_PATH, 'r') as config_file:
+        with open(CONFIG_PATH, 'r', encoding='utf-8') as config_file:
             try:
                 config = json.loads(config_file.read())
             except json.JSONDecodeError as error:
@@ -46,6 +46,7 @@ if __name__ == '__main__':
     parser.add_argument('--frontend_url', type=str, required=False)
     parser.add_argument('--frontend_realm', type=str, required=False)
     parser.add_argument('--exporter', type=str, required=False)
+    parser.add_argument('--prompty', action='store_true', required=False)
     args = parser.parse_args()
 
     if args.backend_url is not None:
@@ -54,5 +55,7 @@ if __name__ == '__main__':
         config["backend_realm"] = args.backend_realm
     if args.exporter is not None:
         config["exporter"] = args.exporter
-
-    run_router(**config)
+    if args.prompty:
+        prompty.run(**config)
+    else:
+        run_router(**config)
